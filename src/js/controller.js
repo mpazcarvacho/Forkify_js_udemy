4daa52952c27722c6acf4550a95e17527ae062c8
mariapaz <mpazcarvacho@gmail.com>
2021-07-13 09:47:58 -0400

    implemented update method for resultsView

--- src/js/controller.js
+++ src/js/controller.js
@@ -24,6 +24,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    //0 - Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     //1 Loading recipe
 
     //async function which will return a promise
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -5,8 +5,8 @@ export default class View {
   _data;
 
   render(data) {
-    if (!data || (Array.isArray(data) && data.length === 0))
-      return this.renderError();
+    // if (!data || (Array.isArray(data) && data.length === 0))
+    //   return this.renderError();
 
     this._data = data;
     const markup = this._generateMarkup();
@@ -30,7 +30,20 @@ export default class View {
 
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
-      console.log(curEl, newEl.isEqualNode(curEl));
+
+      // Updates changed text
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Updating data Attributes
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
     });
   }
 
